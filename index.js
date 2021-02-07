b78c78617f8d10b27d05f87fc8ebc74752f08ee8
John Ratana <jratana1@gmail.com>
2021-02-07 12:40:57 -0500

    commit tree bad677b8997397a5d868260d31cc110f821ac5d2

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase()+string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(string) {
+    let split = string.split(" ")
+    let result= []
+    const list = new Set(["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"])
+    for (let word of split){
+      if (!list.has(word)) {
+        result.push(Formatter.capitalize(word))
+      } else {
+        result.push(word)
+      }
+    }
+    return Formatter.capitalize(result.join(" "))
+  }
+
 }
